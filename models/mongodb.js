c4ed3e4c01a8e1a78cd929fd266d8f2591ac31de
winkwink <amanda.winkler44@gmail.com>
2020-03-19 18:00:11 -0400

    database connection going live, new config

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ColorPicker',{useNewUrlParser: true}, (err)=>{
-    if(!err){
-        console.log('Successfully established connection w mongoDB');
-    }else{
-        console.log('Failed to establish connetion with mongoDB with Error: ' + err);
-    }
-});
+var connectionString= 'mongodb+srv://amanda:<Lollollol12>@cluster0-wcbhc.mongodb.net/test?retryWrites=true&w=majority';
+
+var db = mongoose.connect(connectionString).catch((error) => { console.log(error); });
+
+
+// mongoose.connect('mongodb://localhost:27017/ColorPicker',{useNewUrlParser: true}, (err)=>{
+//     if(!err){
+//         console.log('Successfully established connection w mongoDB');
+//     }else{
+//         console.log('Failed to establish connetion with mongoDB with Error: ' + err);
+//     }
+// });
 
 require('./inventory.model');
